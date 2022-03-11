2f9792ddfea9a55635c5b894a7039a1efe0b3fbc
MD Huq <muhtasimul@outlook.com>
2022-03-10 18:55:33 -0800

    final commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Chatbot from "../components/mainComponent/Chatbot"
+import Head from "next/head"
 
 export default function HomePage() {
 
   return (
     <>
+      <Head>
+        <title>Learn BC Chatbot</title>
+        <meta name="description" content="A chatbot to learn about British Columbia" />
+      </Head>
       <Chatbot />
     </>
   )
